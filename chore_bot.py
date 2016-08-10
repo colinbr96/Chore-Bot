35815fd5c9a3211a290e3d3b7cc5be08af46fb4b
Colin B <kolivbrauv@gmail.com>
2016-08-09 20:10:17 -0700

    Initial release

--- chore_bot.py
+++ chore_bot.py
@@ -1,4 +1,5 @@
 import datetime
+import requests
 
 chores = {
 	'Monday':		'Living Room & Dining Room',
@@ -17,21 +18,36 @@ groups = [
 	'Georgina & Luis'
 ]
 
-todaysGroup = 0
+def load_group() -> int:
+	file = open('group', 'r')
+	group_num = file.read(1)
+	return int(group_num)
+
+def save_group(group_num):
+	file = open('group', 'w')
+	file.write(group_num)
+
+def http_post(text):
+	r = requests.post('https://api.groupme.com/v3/bots/post', data = {
+		'bot_id': 'e7cd7c4d3448e2eadcd0dca190',
+		'text': text
+	})
 
 def run():
-	global todaysGroup
+	todaysGroup = load_group()
 
 	now = datetime.datetime.now()
 	todaysChore = chores[now.strftime('%A')]
 	
-	todaysGroup += 1
-	if todaysGroup > 4:
-		todaysGroup = 1
-	
 	if todaysChore != '':
-		# post on groupme
+		todaysGroup += 1
+		if todaysGroup > 4:
+			todaysGroup = 1
+
+		save_group(todaysGroup)
 		message = '{}, your chore today is: {}'.format(groups[todaysGroup], todaysChore)
-		
+		http_post(message)
+
+
 if __name__ == '__main__':
 	run()
--- http_post.py
+++
@@ -1,9 +0,0 @@
-import urllib
-import urllib2
-
-url = "https://api.groupme.com/v3/bots/post"
-params = urllib.urlencode({
-	"bot_id": "58ae5dfd7dfe0acde6da2340f2",
-	"text": "Test"
-})
-reponse = urllib2.urlopen(url, params).read()
