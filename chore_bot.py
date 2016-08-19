ae65cfa54e18bc2b0cada961247584daf64b58f8
Colin B <kolivbrauv@gmail.com>
2016-08-18 18:41:58 -0700

    Bot must not have posted in last 18 hours
    Fail safe so that the bot can't post unless it's been at  least 18 hours
    since it last posted.

--- chore_bot.py
+++ chore_bot.py
@@ -55,32 +55,39 @@ def bot_post(text, bot_ID):
     })
     print(text)
 
+# Posts to the Notifier Bot
+def debug_post(text):
+    bot_post(text, CONFIG_DATA['notifier_bot'])
+
 # Builds and posts the chore reminder
-def chore_notify(date):
+def chore_notify(now):
     global CONFIG_DATA
 
-    todays_chore = chores[date.strftime('%A')]
+    todays_chore = chores[now.strftime('%A')]
     message = '{}, your chore for today is: the {}\n({})'.format(
         groups[CONFIG_DATA['current_group']-1],
         todays_chore,
-        date.strftime('%a, %b %d, %Y')
+        now.strftime('%a, %b %d, %Y')
     )
 
     bot_post(message, CONFIG_DATA['chore_bot'])
-    bot_post('Chore-Bot posted.', CONFIG_DATA['notifier_bot'])
-
     CONFIG_DATA['current_group'] += 1
+    CONFIG_DATA['last_ran'] = int(now.timestamp())
     save_config_data()
 
-# Runs a chore reminder if weekday
+# Runs a chore reminder if weekday & at least 18 hours since last run
 def run():
     load_config_data()
-
     now = datetime.datetime.now()
+    seconds_since_last_post = int(now.timestamp()) - CONFIG_DATA['last_ran']
+
     if now.weekday() >= 5: # If it is a weekend
-        bot_post('Chore-Bot aborted post: Weekend.', CONFIG_DATA['notifier_bot'])
+        debug_post('Chore-Bot aborted post: Weekend.')
+    elif seconds_since_last_post < 64800: # If it has been at least 18 hours
+        debug_post('Chore-Bot aborted post: Too recently ran: {} seconds ago (< 64800).'.format(seconds_since_last_post))
     else:
         chore_notify(now)
+        debug_post('Chore-Bot posted.')
 
 # ----------------------------------------------------------------------------- EXECUTION
 
