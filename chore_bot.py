e6f19cf590ec03228d333b1774924e6a4c27a73f
Colin B <kolivbrauv@gmail.com>
2016-08-21 13:40:06 -0700

    Fixed global variables' letter case

--- chore_bot.py
+++ chore_bot.py
@@ -7,11 +7,11 @@ import yaml # http://pyyaml.org/
 # ----------------------------------------------------------------------------- DATA & CONSTANTS
 
 # Configuration
-CONFIG_FILE = 'config.yaml'
-CONFIG_DATA = dict()
+config_file = 'config.yaml'
+config_data = dict()
 
 # Chore Schedule
-chores = {
+CHORES = {
     'Monday':   'Living Room & Dining Room',
     'Tuesday':  'Kitchen',
     'Wednesday':    'Bathroom & Outside Garbage',
@@ -20,7 +20,7 @@ chores = {
 }
 
 # Roommate Groups
-groups = [
+GROUPS = [
     'Aaron & Rachel',
     'Colin & Jasper',
     'Christina & Rod',
@@ -29,23 +29,23 @@ groups = [
 
 # ----------------------------------------------------------------------------- FUNCTIONS
 
-# Loads the contents of config.yaml into CONFIG_DATA
+# Loads the contents of config.yaml into config_data
 def load_config_data():
-    global CONFIG_DATA
+    global config_data
 
-    with open(CONFIG_FILE, 'r') as stream:
+    with open(config_file, 'r') as stream:
         try:
-            CONFIG_DATA = yaml.load(stream)
+            config_data = yaml.load(stream)
         except yaml.YAMLError as err:
             print(err)
 
-    if CONFIG_DATA['current_group'] > 4:
-        CONFIG_DATA['current_group'] = 1
+    if config_data['current_group'] > 4:
+        config_data['current_group'] = 1
 
-# Saves the contents of CONFIG_DATA to config.yaml
+# Saves the contents of config_data to config.yaml
 def save_config_data():
-    with open(CONFIG_FILE, 'w') as ostream:
-        yaml.dump(CONFIG_DATA, ostream, default_flow_style=False)
+    with open(config_file, 'w') as ostream:
+        yaml.dump(config_data, ostream, default_flow_style=False)
 
 # Performs an HTTP Post request to GroupMe
 def bot_post(text, bot_ID):
@@ -57,29 +57,29 @@ def bot_post(text, bot_ID):
 
 # Posts to the Notifier Bot
 def debug_post(text):
-    bot_post(text, CONFIG_DATA['notifier_bot'])
+    bot_post(text, config_data['notifier_bot'])
 
 # Builds and posts the chore reminder
 def chore_notify(now):
-    global CONFIG_DATA
+    global config_data
 
-    todays_chore = chores[now.strftime('%A')]
+    todays_chore = CHORES[now.strftime('%A')]
     message = '{}, your chore for today is: the {}\n({})'.format(
-        groups[CONFIG_DATA['current_group']-1],
+        GROUPS[config_data['current_group']-1],
         todays_chore,
         now.strftime('%a, %b %d, %Y')
     )
 
-    bot_post(message, CONFIG_DATA['chore_bot'])
-    CONFIG_DATA['current_group'] += 1
-    CONFIG_DATA['last_ran'] = int(now.timestamp())
+    bot_post(message, config_data['chore_bot'])
+    config_data['current_group'] += 1
+    config_data['last_ran'] = int(now.timestamp())
     save_config_data()
 
 # Runs a chore reminder if weekday & at least 18 hours since last run
 def run():
     load_config_data()
     now = datetime.datetime.now()
-    seconds_since_last_post = int(now.timestamp()) - CONFIG_DATA['last_ran']
+    seconds_since_last_post = int(now.timestamp()) - config_data['last_ran']
 
     if now.weekday() >= 5: # If it is a weekend
         debug_post('Chore-Bot aborted post: Weekend.')
